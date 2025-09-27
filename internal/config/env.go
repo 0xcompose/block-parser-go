872a98bb6591652f0ebefa7799b391dd0c0c802c
slava <v.vladykin@ci-id.net>
2025-09-27 18:34:50 +0300

    feat(config/env): better env retrieval and loading .env only once

--- internal/config/env.go
+++ internal/config/env.go
@@ -1,12 +1,44 @@
 package config
 
-import "os"
+import (
+	"errors"
+	"log"
+	"os"
 
-func GetEnvValueSafely(key string, errorMessage string) string {
+	"github.com/joho/godotenv"
+)
+
+const envFilePath = "../../.env"
+
+// as soon as the package is imported, the .env file is loaded
+func init() {
+	err := godotenv.Load(envFilePath)
+	log.Printf("Loading .env file: %q", envFilePath)
+
+	if err != nil {
+		log.Printf("Error loading .env file: %q", err)
+	}
+
+	log.Print("Successfully loaded .env file")
+}
+
+// returns the value of the environment variable if it exists, otherwise returns an error
+func GetEnv(key string, errorMessage string) (string, error) {
 	value, ok := os.LookupEnv(key)
 
 	if !ok {
-		panic(errorMessage)
+		return "", errors.New(errorMessage)
+	}
+
+	return value, nil
+}
+
+// returns the value of the environment variable if it exists, otherwise returns the default value
+func GetEnvWithDefault(key string, defaultValue string) string {
+	value, ok := os.LookupEnv(key)
+
+	if !ok {
+		return defaultValue
 	}
 
 	return value
--- internal/config/env_test.go
+++ internal/config/env_test.go
@@ -5,13 +5,43 @@ import (
 	"testing"
 )
 
-func TestGetSafeEnvValue(t *testing.T) {
+// TestModuleInitLoadsEnv tests that the `env` module from `config` package loads the environment variables from the .env file
+func TestModuleInitLoadsEnv(t *testing.T) {
+	GetEnv("ALCHEMY_API_KEY", "ALCHEMY_API_KEY is not set")
+
+	if len(os.Getenv("ALCHEMY_API_KEY")) == 0 {
+		t.Errorf("Expected env module to load environment variables, but received empty ALCHEMY_API_KEY")
+	}
+}
+
+func TestGetEnv(t *testing.T) {
 	os.Setenv("TEST_ENV", "test")
 
-	value := GetEnvValueSafely("TEST_ENV", "TEST_ENV is not set")
+	value, err := GetEnv("TEST_ENV", "TEST_ENV is not set")
+
+	if err != nil {
+		t.Errorf("Expected error to be nil, but got '%v'", err)
+	}
 
 	if value != "test" {
 		t.Errorf("Expected value to be 'test', but got '%s'", value)
+	}
+
+	t.Cleanup(os.Clearenv)
+}
+
+func TestGetEnvWithDefault(t *testing.T) {
+	value := GetEnvWithDefault("SOME_RANDOM_ENV", "test")
+
+	if value != "test" {
+		t.Errorf("Expected value to be 'test', but got '%s'", value)
+	}
+
+	os.Setenv("SOME_RANDOM_ENV", "some_value")
+	value = GetEnvWithDefault("SOME_RANDOM_ENV", "test")
+
+	if value != "some_value" {
+		t.Errorf("Expected value to be 'some_value', but got '%s'", value)
 	}
 
 	t.Cleanup(os.Clearenv)
