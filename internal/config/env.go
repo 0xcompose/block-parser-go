50a061053a011efbde4ef93f04495c24cdb467af
slava <v.vladykin@ci-id.net>
2025-09-27 15:23:17 +0300

    feat(config): implement first util for working with env variables

--- internal/config/env.go
+++ internal/config/env.go
@@ -1,1 +1,13 @@
 package config
+
+import "os"
+
+func GetEnvValueSafely(key string, errorMessage string) string {
+	value, ok := os.LookupEnv(key)
+
+	if !ok {
+		panic(errorMessage)
+	}
+
+	return value
+}
